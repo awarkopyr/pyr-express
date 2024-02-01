3f0af4914cd9e579ffe5e41b1125b83bbe935cd2
awarkopyr <awarkohm2002@gmail.com>
2024-02-01 21:44:07 +0600

    app added

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();import {} from 'express';
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+const port = process.env.port || 300;
+
+app.listen(port, () =>{
+    console.log('server running on https://localhost:$(port)');
+})
