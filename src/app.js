b3d408ae0c2ec3a3c5a3a527cd392440dae7cf34
awarkopyr <awarkohm2002@gmail.com>
2024-02-02 02:56:00 +0600

    Pages Added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();import {} from 'express';
+const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
 
-const port = process.env.port || 300;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () =>{
-    console.log('server running on https://localhost:$(port)');
+app.listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
 })
